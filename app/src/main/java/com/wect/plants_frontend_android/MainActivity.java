2edbc8dbd41f7cd4fa3d2b4c192ed43ceee3a9ac
Wect <2602710174@qq.com>
2025-08-09 12:19:19 +0800

    处理 页面适配

--- app/src/main/java/com/wect/plants_frontend_android/MainActivity.java
+++ app/src/main/java/com/wect/plants_frontend_android/MainActivity.java
@@ -8,17 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.wect.plants_frontend_android.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // 不需要额外写 Insets 代码
     }
 }
---
+++ app/src/main/java/com/wect/plants_frontend_android/base/BaseActivity.java
@@ -0,0 +1,54 @@
+package com.wect.plants_frontend_android.base;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.ColorRes;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this); // 开启沉浸式
+        setSystemBarColors(getSystemBarColorRes()); // 设置系统栏颜色
+    }
+
+    /**
+     * 设置状态栏 & 导航栏颜色
+     */
+    protected void setSystemBarColors(@ColorRes int colorRes) {
+        int color = ContextCompat.getColor(this, colorRes);
+        getWindow().setStatusBarColor(color);
+        getWindow().setNavigationBarColor(color);
+    }
+
+    /**
+     * 子类可以重写这个方法，修改系统栏颜色
+     */
+    protected int getSystemBarColorRes() {
+        return android.R.color.white; // 默认白色背景
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+
+        // 自动适配系统栏内边距
+        View rootView = findViewById(android.R.id.content);
+        if (rootView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
+}
